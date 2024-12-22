ef7df723ccffd24a66e749e0c31c78f2bb5874da
alexianen <mitrofanalexia@gmail.com>
2024-12-22 10:08:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+document.getElementById("searchBtn").onclick = function() {
+    var secondpart = document.querySelector(".second-part");
+    if (secondpart.style.display === "none") {
+        secondpart.style.display = "block";
+    } else {
+        secondpart.style.display = "none";
+    }
+};
 
+document.getElementById("seemoreBtn").onclick = function() {
+    let morearticles = document.querySelector(".bonus-articles");
+    if (morearticles.style.display === "none") {
+        morearticles.style.display = "block";
+    } 
+}
